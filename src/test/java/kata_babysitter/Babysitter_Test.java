6af3025ccfbf9d3372cc380f5efcc1f9695f4514
Jed Duffey <jedduffey@gmail.com>
2016-11-23 13:45:14 -0500

    added @Before to tests, created 2nd failing test

--- src/test/java/kata_babysitter/Babysitter_Test.java
+++ src/test/java/kata_babysitter/Babysitter_Test.java
@@ -2,21 +2,29 @@ package kata_babysitter;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Babysitter_Test {
 	
+	private Babysitter testObject;
+	
+	@Before
+	public void executedBeforeEachTest(){
+		//Arrange
+		testObject = new Babysitter();
+	}
+	
 	@Test
 	public void shouldReturnStartTime(){
-		
-		//Arrange
-		Babysitter testObject = new Babysitter();
-		
 		//Act
 		int actual = testObject.returnStartTime(6);
-		
 		//Assert
 		assertEquals(6, actual);
+	}
+	
+	@Test public void shouldReturnStartTimeWithADifferentTime() {
+		assertEquals(7, testObject.returnStartTime(7));
 	}
 
 }
